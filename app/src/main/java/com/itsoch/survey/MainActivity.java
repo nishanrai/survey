32c7b69e3c148483226392c782a21d377eac6b53
inxlu_r <nishanr101@gmail.com>
2017-04-17 13:14:16 +0545

    Checkbox code updated

--- app/src/main/java/com/itsoch/survey/MainActivity.java
+++ app/src/main/java/com/itsoch/survey/MainActivity.java
@@ -3,6 +3,8 @@ package com.itsoch.survey;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +22,41 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        final CheckBox c1 = (CheckBox) findViewById(R.id.checkBox);
+        final CheckBox c2 = (CheckBox) findViewById(R.id.checkBox2);
+        final CheckBox c3 = (CheckBox) findViewById(R.id.checkBox3);
+        final CheckBox c4 = (CheckBox) findViewById(R.id.checkBox4);
+
+        c1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                c2.setChecked(false);
+                c1.setChecked(b);
+            }
+        });
+        c2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                c1.setChecked(false);
+                c2.setChecked(b);
+            }
+        });
+
+        c3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                c4.setChecked(false);
+                c3.setChecked(b);
+            }
+        });
+        c4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                c3.setChecked(false);
+                c4.setChecked(b);
+            }
+        });
     }
+
 }
